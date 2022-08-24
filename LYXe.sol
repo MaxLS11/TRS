88c52a93b1ace99406ec7819c5bcef47a972109c
Maxim <95439259+MaxLS11@users.noreply.github.com>
2022-08-25 00:13:55 +0200

    Update LYXe.sol

--- LYXe.sol
+++ LYXe.sol
@@ -2,36 +2,23 @@
 pragma solidity ^0.8.0;
 
 
-import from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-import from "@Lukso/LSP8IdentifiableDigitalAssetCore.sol";
-import from "@Lukso/LSP4DigitalAssetMetadata/LSP4DigitalAssetMetadata.sol";
-import from "@erc725/smart-contracts/contracts/interfaces/IERC725Y.sol";
+import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import {ERC725YCore} from "@erc725/smart-contracts/contracts/ERC725YCore.sol";
+import {LSP8IdentifiableDigitalAssetCore} from "./LSP8IdentifiableDigitalAssetCore.sol";
+import {LSP4DigitalAssetMetadata} from "../LSP4DigitalAssetMetadata/LSP4DigitalAssetMetadata.sol";
 
 
-contract Trees  is IERC165, IERC725Y, LSP4DigitalAssetMetadata, LSP8IdentifiableDigitalAssetCore{
+import {_INTERFACEID_LSP8} from "./LSP8Constants.sol";
 
-    function supportsInterface(bytes4 interfaceId)public view virtual override(IERC165, ERC725YCore)
-        returns (bool)
+contract Trees is IERC165, ERC725YCore, LSP4DigitalAssetMetadata, LSP8IdentifiableDigitalAssetCore {
+
+    constructor(string memory name_, string memory symbol_, address newOwner_) LSP4DigitalAssetMetadata(name_, symbol_, newOwner_) {
+    
+    
+    } 
+
+    function supportsInterface(bytes4 interfaceId)public view virtual override(IERC165, ERC725YCore)returns (bool)
     {
-        return interfaceId == _INTERFACEID_LSP8 || 
-        super.supportsInterface(interfaceId);
+        return interfaceId == _INTERFACEID_LSP8 || super.supportsInterface(interfaceId);
     }
-}
-
- constructor(string memory name_, string memory symbol_, address newOwner_) LSP4DigitalAssetMetadata(name_, symbol_, newOwner_) {
- 
- 
- 
- 
- 
- }
-
-
-
-
-
-
-
-
-
 }
