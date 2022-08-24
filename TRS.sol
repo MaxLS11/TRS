fa5d4c3b0afd3965e3f93db73a4bff26ddbe2f6c
Maxim <95439259+MaxLS11@users.noreply.github.com>
2022-08-24 23:33:23 +0200

    Update TRS.sol

--- TRS.sol
+++ TRS.sol
@@ -11,7 +11,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract Trees is ERC721, IERC721Enumerable, IERC721Metadata, Ownable {
+contract Trees is ERC721, IERC721Enumerable, IERC721Metadata, Ownable, IERC721Receiver {
     
     using Strings for uint;
     uint public constant maxPurchase = 3;
