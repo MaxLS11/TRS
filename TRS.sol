3f690f4bd13df610ba6a059545ccdc95d4dda653
Maxim <95439259+MaxLS11@users.noreply.github.com>
2022-08-24 08:54:54 +0200

    Update TRS.sol

--- TRS.sol
+++ TRS.sol
@@ -1,62 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
+import "./IERC721.sol";
+import "./IERC721Metadata.sol";
+import "./IERC721Receiver.sol";
+import "./Strings.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
-
-contract Trees is Initializable, ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
-    
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize() initializer public {
-        __ERC721_init("Trees", "TRS");
-        __ERC721Enumerable_init();
-        __ERC721URIStorage_init();
-        __Ownable_init();
-    }
-
-    function safeMint(address to, uint256 tokenId, string memory uri)
-        public
-        onlyOwner
-    {
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
-
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
-}
+contract T is ERC721, ERC721Enumerable, IERC721Metadata {
+    using SafeMath for uint256;
+    uint public constant maxPurchase = 3;
+    uint256 public constant MAX_T = 100;
