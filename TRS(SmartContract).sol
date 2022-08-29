b5a2b781127c716c2fa1c3c4f54942cf2ee27313
Maxim <95439259+MaxLS11@users.noreply.github.com>
2022-08-29 11:51:38 +0200

    Update TRS(SmartContract).sol

--- TRS(SmartContract).sol
+++ TRS(SmartContract).sol
@@ -2,34 +2,38 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 
-contract Trees is IERC721, IERC165, IERC721Enumerable, IERC721Metadata, Ownable {
-    using Strings for uint256;
-    string public baseURI;
-    uint public constant maxPurchase = 3;
-    uint256 public constant MAX_TREES = 100;
-    
-    }
-    constructor (string memory _name, string memory _symbol, string memory baseURI) {
-        Trees = _name;
-        TRS = _symbol;
-        setBaseURI(baseURI);
+    contract Trees is IERC721, ERC165, IERC721Enumerable, IERC721Metadata, Ownable {
+
+        using Strings for uint;
+        uint public constant maxPurchase = 3;
+        uint256 public constant MAX_TREES = 100;
+        mapping(uint => address) _owners;
+        mapping(address => uint) _balances;
+        mapping(uint => address) _tokenApprovals;
+        string public baseURI;
+
+        modifier _requireMinted(uint256 tokenId) {
+        require(_exists(tokenId),"not minted");
+        _; 
+       }
+
+    constructor (string memory _name, string memory _symbol) IERC721(_name, _symbol) {
+        name = _name;
+        symbol = _symbol;
     }
 
         function balanceOf(address owner) public view returns(uint256) {
             require(owner != address(0), "zero address");
             return _balances[owner];
     }
-    
-        function ownerOf(uint tokenId) public view _requireMinted(tokenId) returns(address) {
-            return _owners[tokenId];
-    }
+
         function safeTransferFrom(address from, address to, uint tokenId) public {
             require(_isApprovedOrOwner(msg.sender,tokenId),"not an owner or approved!"); //!public 
             _safeTransfer(from, to, tokenId);
@@ -38,10 +42,29 @@ contract Trees is IERC721, IERC165, IERC721Enumerable, IERC721Metadata, Ownable 
             require (_isApprovedOrOwner(msg.sender, tokenId), "not an owner or approved!");
             _transfer(from, to, tokenId);
     } 
+
+     function _isApprovedOrOwner(address spender, uint tokenId) internal view returns (bool){
+        address owner = ownerOf(tokenId);
+        require(spender == owner) ||
+        isApproveForAll(owner, spender); 
+     }
+
+    function OwnerOf(uint tokenId) public _requireMinted(TokenId) returns (address) {
+        return _owners[tokenId];
+
+    //unction isApprovedForAll(address owner) public view returns (bool) {
+        //return _tokenApprovals[owner];
+    //}
+ 
+    }
         function _safeTransfer(address from, address to, uint256 tokenid) internal {
             _transfer(from, to, tokenId);
             require(_checkOnERC721Received(from, to, tokenId), "non erc721 receiver");
     }
+    function getApproved(uint tokenId) public view _requireMinted(tokenId) returns(address) {
+        return _tokenApprovals(tokenId);
+    }
+
     
     function _checkOnERC721Received(address from, address to, uint256 tokenId) private returns(bool) {
         if(to.code.length > 0) {
@@ -62,7 +85,9 @@ contract Trees is IERC721, IERC165, IERC721Enumerable, IERC721Metadata, Ownable 
         }
     }
     
-    function _transfer(address from,address to, uint256 tokenId) internal {
+
+
+    function _transfer(address from,address to, uint tokenId) internal {
         require(ownerOf(tokenId) == from, "not an owner!");
         require(to != address(0),"to cannot be zero!");
         _beforeTokenTransfer(from, to, tokenId);
@@ -73,12 +98,12 @@ contract Trees is IERC721, IERC165, IERC721Enumerable, IERC721Metadata, Ownable 
         emit Transfer(from, to, tokenId);
         _afterTokenTransfer(from, to, tokenId);   //!
     }
-        function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
+        function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(IERC721, IERC721Enumerable) {
                 super._beforeTokenTransfer(from, to, tokenId);
     } 
         function _afterTokenTransfer(address from, address to, uint256 tokenId) internal virtual {} 
 
-        function supportsInterface(bytes4 interfaceId)public view override(ERC721, ERC721Enumerable)returns (bool) {
+        function supportsInterface(bytes4 interfaceId)public view override(IERC721, IERC721Enumerable)returns (bool) {
             return interfaceId == type(IERC721).interfaceId ||
             super.supportsInterface(interfaceId);
     }
@@ -95,29 +120,48 @@ contract Trees is IERC721, IERC165, IERC721Enumerable, IERC721Metadata, Ownable 
             require(success, "Transfer failed.");
     }
         function _exists(uint256 tokenId) internal view returns(bool) {
-            return _owners[tokenId] != address(0); //proverka vvoda v oborot(est token or no)
-        }
-       
-        }
-    }
+            return _owners[tokenId] != address(0);
+            }
 
-    
-        function tokenURI(uint256 tokenId) public _requireMinted(tokenId) view returns(string memory) {
-            string memory baseURI = _baseURI;
-            return bytes(baseURI).length > 0 ?
-            string(abi.encodePacked(baseURI, tokenId.toString())) :
-            "";
+        function approve(address to, uint tokenId) public {
+            address _owner = ownerOf(tokenId);
+            require(_owner == msg.sender || isApprovedForAll(_owner,msg.sender), "not an owner");
+            require(to != _owner, "cannot approve to self");
+            _tokenApprovals[tokenId] = to;
+            emit Approval(owner, to, tokenId);
+        }
+   
+    function burn(uint tokenId) public virtual {
+
+        require(_isApprovedOrOwner(msg.sender, tokenId), "not an owner");
+        address owner = ownerOf[tokenId];
+        _balances[owner]--;
+        delete _owners[tokenId];
     }
+    function _mint(address to, uint tokenId) internal virtual {
+        require( to != address(0), "to canndo be zero");
+        require(!_exists(tokenId), "already exists" );
+        _owner[tokenId] = to;
+        _balances[to]++;
+    }
+       function _safeMint(address to,uint tokenId) internal virtual {
+           _mint(to, tokenId);
+           require (_checkOnOnERC721Received(msg.sender, to, tokenId), "non ERC721 receiver!");
+       }
+
 
        function _baseURI() internal pure virtual override returns (string memory) {
-           return baseTokenURI;
+           return "";
     }
+        
+       }
 
-       function setBaseURI(string memory _baseTokenURI) public onlyOwner {
-           baseTokenURI = _baseTokenURI;
-}
+       function tokenURI(uint tokenId ) public _requireMinted(tokenId) view returns(string memory) {
 
+           string memory baseURI = _baseURI();
+           return bytes(baseURI).length > 0 ?
+           strung(abi.encodePacked(baseURI, tokenId.toString))) :
+           "";
+       }
 
-
-}
-
+    }
