0ee68ccd8ad876f08f3d5997c306077a7596270c
Maxim <95439259+MaxLS11@users.noreply.github.com>
2022-08-29 07:23:06 +0200

    Update TRS(SmartContract).sol

--- TRS(SmartContract).sol
+++ TRS(SmartContract).sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol.";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
