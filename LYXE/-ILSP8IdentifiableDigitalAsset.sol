2ef5a0f5a23708f8184f07f03fd4133fb5622c78
Maxim <95439259+MaxLS11@users.noreply.github.com>
2022-09-01 23:52:07 +0200

    Update -ILSP8IdentifiableDigitalAsset.sol

--- LYXE/-ILSP8IdentifiableDigitalAsset.sol
+++ LYXE/-ILSP8IdentifiableDigitalAsset.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: CC0-1.0
+pragma solidity ^0.8.0;
 
+
+import {ILSP1UniversalReceiver} from "../LSP1UniversalReceiver/ILSP1UniversalReceiver.sol";
+import {ILSP8IdentifiableDigitalAsset} from "./ILSP8IdentifiableDigitalAsset.sol";
+import {ERC725Y} from "@erc725/smart-contracts/contracts/ERC725Y.sol";
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import {ILSP1UniversalReceiver} from "../LSP1UniversalReceiver/ILSP1UniversalReceiver.sol";
+import {ERC165Checker} from "../Custom/ERC165Checker.sol";
+import {ERC725Y} from "@erc725/smart-contracts/contracts/ERC725Y.sol";
