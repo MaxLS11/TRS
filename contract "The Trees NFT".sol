f2e86993d2d354b44a6274f99c4962478b5a2e04
Maxim <95439259+MaxLS11@users.noreply.github.com>
2022-09-07 19:17:40 +0200

    Update contract "The Trees NFT".sol

--- contract "The Trees NFT".sol
+++ contract "The Trees NFT".sol
@@ -2,152 +2,76 @@
 pragma solidity ^0.8.5;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
 
-contract TheTreesNFT is ERC721, Ownable {
-    address public owner;
-    event ValueReceived(address sender, uint256 amount);
-    using Strings for uint256;
-    uint256 public constant maxSupply = 100;
-    uint256 public constant maxMintAmount = 3;
-    mapping (uint => address) _owners;
-    mapping (address => uint256[]) nftOwner;
-    mapping(address => uint256) _balances;
-    uint256[] soldedTokenIds;
-    uint256 public _treesPrice = 0 LYXe;
-    bool public saleIsActive = true;
-    
-    constructor() ERC721("Trees", "TRS") {
+
+    contract TreesNFT is ERC721, ERC721Enumerable, Ownable {
+
         
-  
-    }
-  
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseTokenURI; 
-    }
+        uint256 public constant maxMintAmount = 1;
+        uint256 public constant max_Supply = 100;
+        using Strings for uint256;
+        uint256 private _treesPrice = 80000000000000000; //test
+        string private baseURI;
+        event ValueReceived(address sender, uint256 amount);
+        bool public saleIsActive = true;
 
-    function setBaseURI(string memory _baseTokenURI) public onlyOwner {
-        baseTokenURI = _baseTokenURI;
-    
-    }
-    
-    function balanceOf(address owner) public view returns(uint256) {
-        require(owner != address(0), "zero address");
-        return _balances[owner];
+    constructor() ERC721("The Trees NFT", "TRS") {
+
     }
     
-    function withdraw() public payable onlyOwner {
-        uint256 balance = address(this).balance;
-        require(balance > 0, "No ether left to withdraw");
-        (bool success, ) = (msg.sender).call{value: balance}("");
-        require(success, "Transfer failed.");
-    }
 
-    modifier checkTokenStatus(uint256 tokenId) {
-        bool isTokenSold = false;
-        for (uint256 i = 0; i < soldedTokenIds.length; i++) {
-        if(soldedTokenIds[i] == tokenId) {
-            isTokenSold = true;
-                break;
-        }
-    }
-            require(!isTokenSold, "Token is sold");
-            _;
-}
-    modifier _requireMinted(uint256 tokenId) {
-        require(_exists(tokenId),"not minted");
-        _;
-    }
-    function _exists(uint256 tokenId) internal view returns(bool) {
-        return _owners[tokenId] != address(0);
-        }
-
-    function tokenURI(uint256 tokenId) public _requireMinted(tokenId) view returns(string memory) {
-        string memory baseURI = _baseURI;
-        return bytes(baseURI).length > 0 ?
-        string(abi.encodePacked(baseURI, tokenId.toString())) :
-        "";
-    }
-
-    function _mintTrees(uint256 numberOfTokens) public payable {
-        require(saleIsActive, "Sale must be active to mint token");
-        require(numberOfTokens <= maxMintAmount, "Can only mint 3 tokens at a time");
-        require(totalSupply().add(numberOfTokens) <= maxSupply, "Purchase would exceed max supply of Trees");
-        require(_treesPrice.mul(numberOfTokens) <= msg.value, "LYXe value sent is not correct");
-        
-        for(uint256 i = 0; i < numberOfTokens; i++) {
-            uint256 mintIndex = totalSupply();
-            if (totalSupply() < maxSupply) {
-                _safeMint(msg.sender, mintIndex);
-            }
-        }
-    }      
-
-
-    function _safeMint(address to,uint256 tokenId) internal virtual {
-            _mint(to, tokenId);
-            require(_checkOnERC721Received(msg.sender, to, tokenId),"non ERC721 receiver!");
-    }
-   
-    function _transfer(address from,address to, uint256 tokenId) internal {
-        require(ownerOf(tokenId) == from, "not an owner!");
-        require(to != address(0),"to cannot be zero!");
-        _beforeTokenTransfer(from, to, tokenId);
-        _balances[from]--;
-        _balances[to]++;
-        _owners[tokenId] = to;
-
-        emit Transfer(from, to, tokenId);
-        _afterTokenTransfer(from, to, tokenId); 
-    }
-        function ownerOf(uint256 tokenId) public view _requireMinted(tokenId) returns(address) {
-            return _owners[tokenId];
-        }
-
-    function _safeTransfer(address from, address to, uint256 tokenId) internal {
-            _transfer(from, to, tokenId);
-            require(_checkOnERC721Received(from, to, tokenId), "non ERC721 receiver");
-    }
-
-    function _checkOnERC721Received(address from, address to, uint256 tokenId) private returns(bool) {
-        if(to.code.length > 0) {
-            try IERC721Receiver(to).onERC721Received(msg.sender, from, tokenId, bytes("")) returns(bytes4 ret) {
-                return ret == IERC721Receiver.onERC721Received.selector;
-            } catch(bytes memory reason) {
-                if(reason.length == 0) {
-                    revert("Non ERC721 receiver!");
-                } else {
-                    assembly {
-                        revert(add(32,reason), mload(reason))
-                    }
-
-                }
-            }
-        } else {
-                return true;
-        }
-    }
-
-  
-       constructor() {
-
-        owner = msg.sender;
-    }
-   
            receive() external payable {
             emit ValueReceived(msg.sender, msg.value);
         }
-        
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool){
         return super.supportsInterface(interfaceId);
-        
     }
+
+	function withdraw() public onlyOwner {
+			uint256 balance = address(this).balance;
+			payable(msg.sender).transfer(balance);
+		}    
+
+    function setPrice(uint256 _newPrice) public onlyOwner() {
+        _treesPrice = _newPrice;
+    }
+
+    function getPrice() public view returns (uint256){
+        return _treesPrice;
+    }
+
+    function mint(uint256 _mintAmount) public payable {
+        uint256 supply = totalSupply();
+        //require(!paused);
+        require(_mintAmount > 0);
+        require(_mintAmount <= maxMintAmount);
+        require(supply + _mintAmount <= max_Supply);
+
+            if (msg.sender != owner()) {
+              require(msg.value >= _treesPrice * _mintAmount);
+    }
+
+    for (uint256 i = 1; i <= _mintAmount; i++) {
+      _safeMint(msg.sender, supply + i);
+    }
+  }
    
-   
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+    
+    function setBaseURI(string memory newBaseURI) public onlyOwner {
+        baseURI = newBaseURI;
+    }
+
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
         override(ERC721, ERC721Enumerable)
@@ -155,13 +79,8 @@ contract TheTreesNFT is ERC721, Ownable {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
-    function _afterTokenTransfer(address from, address to, uint 256 tokenId) {}
-   
-   
-   
-   
-   
-   
-}
-   
+    function flipSaleState() public onlyOwner {
+        saleIsActive = !saleIsActive;
+    }
+
 }
