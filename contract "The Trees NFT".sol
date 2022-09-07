e472069ef0ced5047ac65f5bd627ac767ecea1c0
Maxim <95439259+MaxLS11@users.noreply.github.com>
2022-09-07 19:30:54 +0200

    Update contract "The Trees NFT".sol

--- contract "The Trees NFT".sol
+++ contract "The Trees NFT".sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.5;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
